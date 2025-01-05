d36c62bda0b65f133e5d6978acd96f5614dc799a
KangXunWong <97664140+KangXunWong@users.noreply.github.com>
2025-01-05 21:02:09 +0900

    changed the frontend url to reach backend to be dynamic

--- coinchange-frontend/src/apiService.js
+++ coinchange-frontend/src/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/coin-change";
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const calculateCoinChange = async (targetAmount, coinDenominations) => {
   try {
